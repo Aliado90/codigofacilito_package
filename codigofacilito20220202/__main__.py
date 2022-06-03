84ff3d94d93e754440f2ff17acdece8c50714e31
Aliado90 <43979035+Aliado90@users.noreply.github.com>
2022-06-02 21:56:28 -0500

    Modulo logging

--- codigofacilito20220202/__main__.py
+++ codigofacilito20220202/__main__.py
@@ -1,6 +1,19 @@
+import logging
+
 from codigofacilito20220202 import unreleased
 
+"""
+INFO - 10
+DEBUG - 20
+WARNING - 30
+ERROR - 40
+CRITICAL - 50
+"""
+
+logging.basicConfig(level=logging.DEBUG)
+
 if __name__ == '__main__':
-    print('Estamos probando la ejecución del paquete')
+    logging.debug('Estamos iniciando la ejecución del paquete')
     workshops = unreleased()
-    print(workshops)
+    logging.debug(workshops)
+    logging.debug('Estamos finalizando la ejecución del paquete')
